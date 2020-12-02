9bec7143de3965dbbdade6bda5f35388b3965e17
Elmira Baltinova <62242594+EBaltinova@users.noreply.github.com>
2020-12-02 14:18:18 +0600

    Задание №17 (заново)

--- src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -42,5 +42,12 @@ public class ApplicationManager {
         driver.quit();
     }
 
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 
 }
